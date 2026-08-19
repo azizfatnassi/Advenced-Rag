400f71503552376855c373d0b703844fe41f9553
azizfatnassi <mohamedaziz.fatnassi@esprit.tn>
2026-08-19 18:33:41 +0100

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,31 +1,28 @@
-
-
-#from fastapi import FastAPI
-#from app.api.routes import router
-
-#app = FastAPI()
-#app.include_router(router)
-
-
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.staticfiles import StaticFiles
 from app.api.routes import router
 
-
 @asynccontextmanager
-async def lifespan(app:FastAPI):
+async def lifespan(app: FastAPI):
     from app.rag.reranker import rerank
     from langchain_core.documents import Document
     dummy = [Document(page_content="warmup", metadata={})]
-    rerank("warmup",dummy,top_k=1)
+    rerank("warmup", dummy, top_k=1)
     print("reranker warmed up")
     yield
 
+app = FastAPI(docs_url=None, lifespan=lifespan)
 
-app = FastAPI(docs_url=None, lifespan=lifespan)  # disable default docs
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 @app.get("/docs", include_in_schema=False)
@@ -36,4 +33,3 @@ async def custom_swagger_ui():
         swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
     )
-
